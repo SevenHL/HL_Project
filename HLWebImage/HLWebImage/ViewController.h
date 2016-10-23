dfd196d844ffc15ac7f7bd3959e38c770ec9814b
Mr.Luo <463377013@qq.com>
2016-10-23 14:20:18 +0800

    创建项目

---
+++ HLWebImage/HLWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLWebImage
+//
+//  Created by 罗浩林 on 16/10/23.
+//  Copyright © 2016年 罗浩林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLWebImage/HLWebImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HLWebImage
+//
+//  Created by 罗浩林 on 16/10/23.
+//  Copyright © 2016年 罗浩林. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
